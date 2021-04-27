b46380fa2dabffab8c89f26a22d4e766e6e58299
Carlton Joseph <carlton.joseph@gmail.com>
2021-04-26 17:20:31 -0700

    add selector family code refactor

--- src/EditProperties.tsx
+++ src/EditProperties.tsx
@@ -6,74 +6,49 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react';
-import { selector, useRecoilState } from 'recoil';
+import { selectorFamily, useRecoilState, useRecoilValue } from 'recoil';
 import {
   selectedElementState,
   elementState,
-  Element,
 } from './components/Rectangle/Rectangle';
+import _ from 'lodash';
+import produce from 'immer';
 
-const selectedElementProperties = selector<Element | undefined>({
-  key: 'selectedElementProperties',
-  get: ({ get }) => {
-    const selectedElementId = get(selectedElementState);
-    if (selectedElementId == null) return;
-    return get(elementState(selectedElementId));
+const editPropertyState = selectorFamily<number, { path: string; id: number }>({
+  key: 'editProperty',
+  get: ({ path, id }) => ({ get }) => {
+    const element = get(elementState(id));
+    return _.get(element, path);
   },
-  set: ({ get, set }, newElement) => {
-    if (!newElement) return;
-    const selectedElementId = get(selectedElementState);
-    if (selectedElementId == null) return;
-    set(elementState(selectedElementId), newElement);
+  set: ({ path, id }) => ({ get, set }, newValue) => {
+    const element = get(elementState(id));
+    const newElement = produce(element, (draft) => {
+      _.set(draft, path, newValue);
+    });
+    set(elementState(id), newElement);
   },
 });
-
 export const EditProperties = () => {
-  const [element, setElement] = useRecoilState(selectedElementProperties);
-  if (!element) return null;
-  const setPosition = (property: 'top' | 'left', value: number) => {
-    setElement({
-      ...element,
-      style: {
-        ...element.style,
-        position: { ...element.style.position, [property]: value },
-      },
-    });
-  };
-  const setSize = (property: 'height' | 'width', value: number) => {
-    setElement({
-      ...element,
-      style: {
-        ...element.style,
-        size: { ...element.style.size, [property]: value },
-      },
-    });
-  };
+  const selectedElement = useRecoilValue(selectedElementState);
+  if (selectedElement == null) return null;
   return (
     <Card>
       <Section heading="Position">
-        <Property
-          label="Top"
-          value={element.style.position.top}
-          onChange={(top) => setPosition('top', top)}
-        />
+        <Property label="Top" path="style.position.top" id={selectedElement} />
         <Property
           label="Left"
-          value={element.style.position.left}
-          onChange={(left) => setPosition('left', left)}
+          path="style.position.left"
+          id={selectedElement}
         />
       </Section>
       <Section heading="Size">
         <Property
           label="Height"
-          value={element.style.size.height}
-          onChange={(height) => setSize('height', height)}
+          path="style.size.height"
+          id={selectedElement}
         />
-        <Property
-          label="Width"
-          value={element.style.size.width}
-          onChange={(width) => setSize('width', width)}
-        />
+        selected
+        <Property label="Width" path="style.size.width" id={selectedElement} />
       </Section>
     </Card>
   );
@@ -90,20 +65,21 @@ const Section: React.FC<{ heading: string }> = ({ heading, children }) => {
 
 const Property = ({
   label,
-  value,
-  onChange,
+  path,
+  id,
 }: {
   label: string;
-  value: number;
-  onChange: (value: number) => void;
+  path: string;
+  id: number;
 }) => {
+  const [value, valueSet] = useRecoilState(editPropertyState({ path, id }));
   return (
     <div>
       <Text fontSize="14px" fontWeight="500" mb="2px">
         {label}
       </Text>
       <InputGroup size="sm" variant="filled">
-        <NumberInput value={value} onChange={(_, value) => onChange(value)}>
+        <NumberInput value={value} onChange={(_, v) => valueSet(v)}>
           <NumberInputField borderRadius="md" />
           <InputRightElement
             pointerEvents="none"
